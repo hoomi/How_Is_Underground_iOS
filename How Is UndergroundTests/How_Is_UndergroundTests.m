ec359cb1ebfea19f4355a00dca45d515b6b6a3f7
hoomi <hoomi2010@gmail.com>
2014-03-17 13:49:29 +0000

    Initial Commit

---
+++ How Is UndergroundTests/How_Is_UndergroundTests.m
@@ -0,0 +1,34 @@
+//
+//  How_Is_UndergroundTests.m
+//  How Is UndergroundTests
+//
+//  Created by Hooman Ostovari on 17/03/2014.
+//  Copyright (c) 2014 Hooman Ostovari. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface How_Is_UndergroundTests : XCTestCase
+
+@end
+
+@implementation How_Is_UndergroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
